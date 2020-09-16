728440ce9daeb65870b3bf7bb787ffa4ee78390b
Viacheslav <viacheslav.sheviakov@gmail.com>
2020-09-17 02:27:11 +0300

    INITIALIZATION: Added pet-project

---
+++ first_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ first_app/lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/ui/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Home Page'),
+    );
+  }
+}
+
